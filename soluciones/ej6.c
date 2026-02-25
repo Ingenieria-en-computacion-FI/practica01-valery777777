efc14894dd0eb2fa19152c605da1274408cbac76
valery777777 <valery.padper@gmail.com>
2026-02-24 20:48:29 -0600

    Update comments in ej6.c for better understanding
    Corrected comments for clarity and completeness.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -13,5 +13,11 @@ int main(){
 }
 //1. Imprime direcciones de todos los elementos.
 //2. Determina si almacenamiento es por filas o columnas.
-//Es por filas, primero se lamacena m[0][0], luego m[0][1], m[0][2], m[0][3], m[1][0], m[1][2] y finalmente
+//Es por filas, primero se lamacena m[0][0], luego m[0][1], m[0][2], m[0][3], m[1][0], m[1][2], m[1][3] y así sucesivamente.
+
 //3. Deduce fórmula de dirección de m[i][j].
+//Dirección: Base + [(i × Número de Columnas) + j] × tam_tipo 
+//Primero, i × Número de Columnas, para calcular cuántos elementos hay en todas las 
+//filas anteriores. Luego + j, avanza dentro de la fila actual hasta la columna que quiero, 
+//eso me da el número total de elementos antes de m[i][j]. Después se multiplica por el 
+//tamaño del tipo de dato y al final se suma a la base.
