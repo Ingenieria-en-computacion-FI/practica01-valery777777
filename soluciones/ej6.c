e8aae1c2b2926503b738b0808c6b3f1cb07a8c21
valery777777 <valery.padper@gmail.com>
2026-02-24 20:27:56 -0600

    Implement memory address printing for 2D array
    Added nested loops to print memory addresses of a 2D array.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,13 @@
-
 #include <stdio.h>
+#define pf printf
+
 int main(){
-    // Resolver ejercicio 6
-    return 0;
+    int m[2][4];
+    
+    for (int i = 0; i < 2; i++){
+        for (int j=0; j <4; j++){
+            pf("La dirección de m[%i][%i] es: %p\n", i,j, &m[i][j]);
+        }
+    }
+    
 }
