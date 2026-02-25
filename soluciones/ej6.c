fe9dee6b36aad684c98477e825cf59a1f5ff0f62
valery777777 <valery.padper@gmail.com>
2026-02-24 20:45:29 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -11,3 +11,7 @@ int main(){
     }
     
 }
+//1. Imprime direcciones de todos los elementos.
+//2. Determina si almacenamiento es por filas o columnas.
+//Es por filas, primero se lamacena m[0][0], luego m[0][1], m[0][2], m[0][3], m[1][0], m[1][2] y finalmente
+//3. Deduce fórmula de dirección de m[i][j].
